e713afe5c4faef561769fe45b3733f827a885947
fadilkun45 <indocrafter76@gmail.com>
2023-07-17 03:51:12 +0700

    ketinggalan

--- src/components/DetailCard.tsx
+++ src/components/DetailCard.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, CardBody, CardHeader, Heading, Text, VStack, Card, useToast, HStack } from '@chakra-ui/react'
 import { Detail } from '../redux/feature/detail'
 import { DeleteIcon } from "@chakra-ui/icons"
+import { formatRupiah } from '../lib/Formatter'
 
 export const DetailCard = ({item, handleClick}: {item: Detail, handleClick: (params: any) => any}) => {
   return (
@@ -9,7 +10,7 @@ export const DetailCard = ({item, handleClick}: {item: Detail, handleClick: (par
         {item.title}
     </Heading>
     <Text pt='2' fontSize='sm'>
-       {item.amount}
+       {formatRupiah(item.amount || 0)}
     </Text>
     <Text pt='2' fontSize='sm'>
        {item.createdAt}
