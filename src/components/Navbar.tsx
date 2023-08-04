6f83a23ac9b17fd193595e5bed1ce121e93b385b
fadilkun45 <indocrafter76@gmail.com>
2023-08-05 02:40:50 +0700

    dev: navbar improvement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,62 @@
 
 
-import { useState,useEffect } from "react"
+import { useState, useEffect, useLayoutEffect } from "react"
 import { DownloadIcon } from "@chakra-ui/icons"
-import { HStack, Spacer, Text,VStack, useToast } from "@chakra-ui/react"
+import { HStack, Spacer, Text, VStack, useToast } from "@chakra-ui/react"
 
 export const Navbar = () => {
-    const toast = useToast()
-    const [supportsPWA, setSupportsPWA] = useState(false);
-    const [promptInstall, setPromptInstall] = useState<any>();
-  
-    useEffect(() => {
-      const handler = (e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-        setSupportsPWA(true);
-        setPromptInstall(e);
-      };
-      window.addEventListener("beforeinstallprompt", handler);
-    }, []);
+  const toast = useToast()
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState<any>();
+  const [PwaInstalled, setPwaInstalled] = useState(false)
 
-  
-    const onClick = (evt: { preventDefault: () => void; }) => {
-      evt.preventDefault();
+  useEffect(() => {
+    const handler = (e: { preventDefault: () => void; }) => {
+      e.preventDefault();
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+  }, []);
 
-      if (!supportsPWA) {
-        toast({
-            title: 'Upss Device Mu tak support PWA',
-            position: 'top-right',
-            isClosable: true,
-            duration: 1000,
-            colorScheme: "red",
-          })
+
+  const onClick = (evt: { preventDefault: () => void; }) => {
+    evt.preventDefault();
+
+    if (!supportsPWA) {
+      toast({
+        title: 'Upss Device Mu tak support PWA',
+        position: 'top-right',
+        isClosable: true,
+        duration: 1000,
+        colorScheme: "red",
+      })
     }
 
-      if (!promptInstall) {
-        return;
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+
+  useLayoutEffect( () => {
+    const relatedApps = navigator.getInstalledRelatedApps();
+    setPwaInstalled(relatedApps.length > 0)
+  }, [])
+
+
+
+
+  return (
+    <HStack marginBottom="16">
+      <Text fontWeight="bold" fontSize="xl">Pencatat Keuangan</Text>
+      <Spacer />
+      {
+        !PwaInstalled ?   <VStack onClick={onClick} >
+        <DownloadIcon boxSize={3} />
+        <Text fontSize="xs">Install App</Text>
+      </VStack> : ""
       }
-      promptInstall.prompt();
-    };
-
- 
-
-
-    return (
-        <HStack marginBottom="16">
-            <Text fontWeight="bold" fontSize="xl">Pencatat Keuangan</Text>
-            <Spacer />
-            <VStack onClick={onClick}>
-                <DownloadIcon boxSize={3} />
-                <Text fontSize="xs">Install App</Text>
-            </VStack>
-        </HStack>
-    )
+    </HStack>
+  )
 }
